34d0ddc706cda83746737737af156aac86f786b2
Jules Junior Meva'a <mevaajules9@gmail.com>
2020-03-23 01:15:37 +0100

    menu improvement

--- seafly/pages/admin.py
+++ seafly/pages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Pages
 
 # Register your models here.
+admin.site.site_header = 'Page d\'administration'
+admin.site.register(Pages)
--- seafly/pages/models.py
+++ seafly/pages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
 
@@ -7,8 +8,8 @@ class Pages(models.Model):
     title = models.CharField(max_length=200, unique=True)
     imgTop = models.CharField(max_length=200)
     imgRight = models.CharField(max_length=200)
-    content = models.CharField(max_length=200)
-    visibleImgLeft = models.BooleanField(default=True)
+    content = models.CharField(max_length=200, default=' ')
+    visibleImgRight = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
