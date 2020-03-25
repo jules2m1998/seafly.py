b74370c0101116c147855954db6e17b6a5027dad
Jules Junior Meva'a <mevaajules9@gmail.com>
2020-03-25 18:02:47 +0100

    ajout de la map

--- seafly/static/pages/js/app.js
+++ seafly/static/pages/js/app.js
@@ -1,11 +1,22 @@
 let $map = document.querySelector("#map");
+let key = 'AIzaSyDqds4AcbBtmntJedcFGT9ZVbZ6OuFDa5Q';
 class GoogleMap {
     /**
      * Charge la carte sur un élément
      * @param element
      */
     load (element){
-
+        $script('https://maps.googleapis.com/maps/api/js?key=' + key + '&callback=initMap', function(){
+            let center = {lat: 13.7771722, lng: 100.5760527},
+            map = new google.maps.Map(element, {
+              center: center,
+              zoom: 13
+            });
+            let marker = new google.maps.Marker({
+                position: center,
+                map: map
+            })
+        })
     }
 }
 
